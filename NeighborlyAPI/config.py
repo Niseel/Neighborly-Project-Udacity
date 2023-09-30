1271187cf809dc3123f266ac87361a096c4ed4f9
Thanh NguyenCong <nguyencongthanh200199@gmail.com>
2023-10-01 02:31:23 +0700

    Submid Project 2 / Neighborly - Udacity

---
+++ NeighborlyAPI/config.py
@@ -0,0 +1,4 @@
+MONGODB_CONNECTION_STRING = "mongodb://thanhnc40-project-2-cosmos:inHrQd6q1PCl2JpYRLloNIbsgytfUGze4DqxalU5PHXYbNM6uwV70TKyGNw6V4L1h73qrwSdJdRdACDbJIzuPg==@thanhnc40-project-2-cosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@thanhnc40-project-2-cosmos@"
+MONGODB_NAME = "thanhnc40-db"
+POSTS_COLLECTION = "posts"
+ADS_COLLECTION = "advertisement"
